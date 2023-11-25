4711385c21202e9f82ebe8bd9cdad02a60b00dad
Patryk Palej <patrykpalej@gmail.com>
2023-11-25 12:16:53 +0100

    Update Mieszkania.py

--- src/dashboard/pages/Mieszkania.py
+++ src/dashboard/pages/Mieszkania.py
@@ -1,9 +1,8 @@
 import streamlit as st
 from datetime import date, timedelta
 
-from dashboard.functions.data_loading import load_data_concurrently
-
-from dashboard.functions.apartments import (
+from functions.data_loading import load_data_concurrently
+from functions.apartments import (
     plot_all, plot_by_month, plot_by_province, plot_map)
 
 
